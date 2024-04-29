b3b4cbf4bc50f99be04a12b5219f237fce4b67d7
Bruno Soares <fc5700@alunos.fc.ul.pt>
2024-04-29 18:45:32 +0100

    test

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const environment = require("./environments/environment");
 const websiteRouter = require("./routes/website-route");
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://appserver.alunos.di.fc.ul.pt:3034/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
