a1e82252719b0b4c9a65be38f1f2f7c4477433fc
Bruno Soares <fc5700@alunos.fc.ul.pt>
2024-04-29 01:38:45 +0100

    change sort headers

--- backend/src/environments/environment.js
+++ backend/src/environments/environment.js
@@ -1,6 +1,6 @@
 const environment = {
     port: 3000,
-    mongo_uri: "mongodb://localhost:5050",
+    mongo_uri: "mongodb://psi034:psi034@localhost:27017/psi034?retryWrites=true&authSource=psi034",
 };
 
 module.exports = environment;
