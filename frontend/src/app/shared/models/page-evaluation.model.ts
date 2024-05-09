59f078cb87b748bae81221eaf0b9846a9ce2c39f
Joao Azevedo <fc56913@alunos.fc.ul.pt>
2024-05-09 16:14:18 +0100

    page-evaluation frontend adicionados

---
+++ frontend/src/app/shared/models/page-evaluation.model.ts
@@ -0,0 +1,8 @@
+export interface PageEvaluation{
+    modules : [
+        {
+            module:String;
+            fail_levels:String[]
+        }
+    ]
+}
--- frontend/src/app/shared/models/page.model.ts
+++ frontend/src/app/shared/models/page.model.ts
@@ -1,7 +1,10 @@
+import { PageEvaluation } from "./page-evaluation.model";
+
 export interface Page {
     _id?: string;
     relativePath: string;
     status?: PageStatus;
+    evaluation: PageEvaluation;
     registryDate?: Date;
     lastEvaluationDate?: Date
   }
