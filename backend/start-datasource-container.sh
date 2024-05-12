6376b82f71d4e4e9b9153ef4911321aa330fc664
AnaMorgado <fc56957@alunos.fc.ul.pt>
2024-05-12 18:23:06 +0100

    .

--- backend/start-datasource-container.sh
+++
@@ -1,7 +0,0 @@
-CONTAINER_NAME="mongoDB"
-
-if docker inspect $CONTAINER_NAME >/dev/null 2>&1; then
-    docker start $CONTAINER_NAME
-else
-    docker run -d -p 5050:27017 --name $CONTAINER_NAME mongo:5.0.6
-fi
