123bbdc77db74e17a3bea481805db1e5bdcac48a
AnaMorgado <fc56957@alunos.fc.ul.pt>
2024-05-21 13:49:01 +0100

    mudar de branch

--- frontend/src/app/page-detail/page-detail.component.ts
+++ frontend/src/app/page-detail/page-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Page } from '../shared/models/page.model';
+import { TableElememt } from '../website-detail/website-detail.component';
+import { WebsiteService } from '../core/website.service';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-page-detail',
@@ -6,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-detail.component.sass']
 })
 export class PageDetailComponent {
+  displayedColumns: string[] = ['total', 'percentagem', 'type'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data: TableElememt[] = [];
+  page ?: Page;
 
+  constructor(
+    private webService: WebsiteService,
+    @Inject(MAT_DIALOG_DATA) public pageId: string
+  ){}
+
+
+  getPage(){
+    if (!this.pageId) return;
+    this.webService.getPage(this.pageId)
+    .subscribe(page =>{
+      this.page = page;
+      this.getData(page);
+    });
+  }
+  
+  getData(page: Page) {
+    throw new Error('Method not implemented.');
+  }
 }
