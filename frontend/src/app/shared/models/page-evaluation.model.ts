b4c4ab62c3923154a7d523ee3c767d5cb62d0f06
Joao Azevedo <fc56913@alunos.fc.ul.pt>
2024-05-23 18:51:55 +0100

    mudança no page-evaluation frontend

--- frontend/src/app/shared/models/page-evaluation.model.ts
+++ frontend/src/app/shared/models/page-evaluation.model.ts
@@ -4,5 +4,26 @@ export interface PageEvaluation{
             module:String;
             fail_levels:String[]
         }
+    ],
+    testes_info?:[
+        {
+            module?: String,
+            tests?:
+            [
+                {
+                    verdict?: String,
+                    identificador?: String
+                }
+            ]
+        }
+    ],
+    percentagens?:[
+        {
+            passed?:Number,
+            warning?: Number,
+            failed?: Number,
+            inapplicable?: Number,
+        }
+
     ]
 }
