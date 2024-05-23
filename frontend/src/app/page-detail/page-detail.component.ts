4dc46005bc72ed3a2f71182090e7ae5b7c8ff448
AnaMorgado <fc56957@alunos.fc.ul.pt>
2024-05-23 18:38:13 +0100

    adicionado filtros

--- frontend/src/app/page-detail/page-detail.component.ts
+++ frontend/src/app/page-detail/page-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { Page } from '../shared/models/page.model';
+import { Page, PageStatus } from '../shared/models/page.model';
 import { TableElememt } from '../website-detail/website-detail.component';
 import { WebsiteService } from '../core/website.service';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
@@ -11,17 +11,47 @@ import { Website } from '../shared/models/website.model';
   templateUrl: './page-detail.component.html',
   styleUrls: ['./page-detail.component.sass']
 })
+
+
+
 export class PageDetailComponent {
+  
+  TypeOfTests = {
+    ACT : "Regra ACT",
+    WCAG : "Técnica WCAG"
+ };
+
+   
+ TypeOfResults = {
+    PASSED : "Passado",
+    WARNNG : "Aviso",
+    FAILED : "Falhado",
+    NOTAPPLIABLE : "Não aplicável"
+  }
+
+  ResultsLevels = {
+    A : "A",
+    AA : "AA",
+    AAA : "AAA",
+  }
+
+
   displayedColumns: string[] = ['total', 'percentagem', 'type'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
   data: TableElememt[] = [];
   page ?: Page;
+  protected activeFilter: string[] = [];
+  testToBePresented: any;
+  tests: any;
+  filters = [{ status:  PageStatus.REGISTERED },
+             { status:  PageStatus.NON_COMPLIANT}
+  ];
 
   constructor(
     private webService: WebsiteService,
     @Inject(MAT_DIALOG_DATA) public info: [string, Website]
   ){}
-
+  
   async ngOnInit(): Promise<void> {
     this.getPage()
   }
@@ -35,4 +65,13 @@ export class PageDetailComponent {
     throw new Error('Method not implemented.');
   }
   
+  clearFilters() {
+    this.activeFilter = this.activeFilter.filter(filter => filter !== status)
+    this.testToBePresented = this.tests;
+  }
+  
+  onFilterChange(status: string) {
+    this.activeFilter?.push(status);
+    this.testToBePresented = this.tests.filter((test: { status: string; }) => status === test.status);
+  }
 }
